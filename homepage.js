e25ee2196d06914b81588ccb3a3566dd70ab52ec
emilykrohn <emilykrohn2@gmail.com>
2026-09-24 15:24:19 -0700

    Use tesseract.js to get and display text from image

--- homepage.js
+++ homepage.js
@@ -1,7 +1,10 @@
-document.forms["upload-form"].addEventListener("submit", (e) => {
+document.forms["upload-form"].addEventListener("submit", async (e) => {
     e.preventDefault();
     const file = document.getElementById("uploaded-image").files[0];
     if (!file) return;
     const img = document.getElementById("card-img");
     img.src = URL.createObjectURL(file);
+
+    const { data: { text } } = await Tesseract.recognize(file, "jpn");
+    document.getElementById("card-text").innerText = text;
 });
