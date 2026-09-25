8bcefdcd1a77b4fa9b5622cb75de5748c1624c20
emilykrohn <emilykrohn2@gmail.com>
2026-09-25 09:18:18 -0700

    Use js to display all words as different span elements

--- homepage.js
+++ homepage.js
@@ -7,7 +7,7 @@ document.forms["upload-form"].addEventListener("submit", async (e) => {
 
     const { data: { text } } = await Tesseract.recognize(file, "jpn");
     const cleanText = text.replace(/\s+/g, "");
-    document.getElementById("card-text").innerText = cleanText;
+    //document.getElementById("card-text").innerText = cleanText;
     kuromoji.builder({ dicPath: "https://cdn.jsdelivr.net/npm/kuromoji@0.1.2/dict/" }).build(function (err, tokenizer) {
         if (err) {
             console.error(err);
@@ -15,5 +15,13 @@ document.forms["upload-form"].addEventListener("submit", async (e) => {
         }
         var path = tokenizer.tokenize(cleanText);
         console.log(path);
+        
+        for (const word of path) {
+            const newWord = document.createElement('span');
+            const text = Object.entries(word)[10][1];
+            newWord.textContent = text;
+            newWord.classList.add('card-word');
+            document.getElementById("card-text").appendChild(newWord);
+        }
     });
 });
