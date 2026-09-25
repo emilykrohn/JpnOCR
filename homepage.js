5ae061b6b8933d21c62431c10a2976f7020452bc
emilykrohn <emilykrohn2@gmail.com>
2026-09-25 15:25:32 -0700

    Display definition of word mouse is hovering over

--- homepage.js
+++ homepage.js
@@ -1,3 +1,4 @@
+
 document.forms["upload-form"].addEventListener("submit", async (e) => {
     e.preventDefault();
     const file = document.getElementById("uploaded-image").files[0];
@@ -37,3 +38,48 @@ document.forms["upload-form"].addEventListener("submit", async (e) => {
         }
     });
 });
+
+const res = await fetch("./jmdict-eng-common-3.6.2.json");
+const json = await res.json();
+const index = new Map();
+json.words.forEach((word, i) => {
+    // Create array of readings for word that will look like ["食べる", "たべる"]
+    const readings = [
+        ...word.kanji.map(k => k.text),
+        ...word.kana.map(k => k.text),
+    ];
+
+    for (const reading of readings) {
+        // if reading isn't in index than add new reading to index
+        if (!index.has(reading)) {
+            index.set(reading, []);
+        }
+        // push index to the value of the key for reading ex: "はし": [1, 15, 32]
+        index.get(reading).push(i);
+    }
+});
+
+let current_word = "";
+
+document.addEventListener('mousemove', e => {
+    const element = document.elementFromPoint(e.clientX, e.clientY);
+    if (element.nodeName && element.nodeName.toLowerCase() === 'span' && element.innerHTML !== current_word) {
+        current_word = element.innerHTML;
+        displayDefinition();
+    }
+})
+
+function lookup(term) {
+    const wordIndices = index.get(term) || [];
+    return wordIndices.map(i => json.words[i]);
+}
+
+function getGlosses(term) {
+    return lookup(term).flatMap(word =>
+        word.sense.flatMap(s => s.gloss.map(g => g.text))
+    );
+}
+
+function displayDefinition() {
+    document.getElementById('definition').innerHTML = getGlosses(current_word);
+}
