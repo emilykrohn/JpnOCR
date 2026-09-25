739102faf9619705ac4142eccfa662969db3d4f4
emilykrohn <emilykrohn2@gmail.com>
2026-09-25 09:43:33 -0700

    Words are color coded based on the parts of speech

--- homepage.js
+++ homepage.js
@@ -18,9 +18,21 @@ document.forms["upload-form"].addEventListener("submit", async (e) => {
         
         for (const word of path) {
             const newWord = document.createElement('span');
+            const pos = Object.entries(word)[4][1];
             const text = Object.entries(word)[10][1];
             newWord.textContent = text;
             newWord.classList.add('card-word');
+            if (pos === "名詞") {
+                newWord.classList.add('red-text');
+            } else if (pos === "助詞") {
+                newWord.classList.add('blue-text');
+            } else if (pos === "動詞") {
+                newWord.classList.add("orange-text");
+            } else if (pos === "助動詞") {
+                newWord.classList.add("green-text");
+            } else if (pos === "副詞") {
+                newWord.classList.add("purple-text");
+            }
             document.getElementById("card-text").appendChild(newWord);
         }
     });
